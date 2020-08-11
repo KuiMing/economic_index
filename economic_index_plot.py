56b5df5507c5142e0ff4325aa903056efc74ef3e
KuiMing <benjamin0901@gmail.com>
2020-08-11 13:25:35 +0800

    typo

--- economic_index_plot.py
+++ economic_index_plot.py
@@ -3,4 +3,4 @@ from plotly.offline import plot
 import plotly.express as px
 index_archive = pd.read_csv('economic_index_adjusted.csv')
 fig = px.line(index_archive, x="datetime", y="economic_index", color='economic')
-plot(fig, filename='ecnomic_index.html')
+plot(fig, filename='economic_index.html')
